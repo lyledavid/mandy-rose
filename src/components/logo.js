261c5c7ce0a9acdb5cc182c6eb9fa190b06bdb53
Lyle David <e-lyle.david@msc.com>
2023-02-22 22:17:53 +0000

    style changes

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
-import rose from "../../content/rose.svg";
+import rose from "../../content/rose.svg"
 
 import { siteShortTitle } from "../../config"
 
@@ -9,7 +9,7 @@ const StyledLogo = styled.div`
   position: relative;
   z-index: 9;
 
-  font-family: 'Gloock', serif;
+  font-family: "Gloock", serif;
   font-size: ${({ size }) => (size ? size : "1.75rem")};
   font-weight: 600;
   color: ${({ theme, color }) => theme.colors[color] || color};
@@ -24,7 +24,7 @@ const Logo = ({ size, color }) => (
   <StyledLogo color={color} size={size}>
     {/* {siteShortTitle} */}
     Mandy
-    <img src={rose} alt="Your SVG" height={40} style={{marginBottom: -2}} />
+    <img src={rose} alt="Your SVG" height={40} style={{ marginBottom: -2 }} />
     Rose
   </StyledLogo>
 )
